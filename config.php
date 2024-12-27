c11242151dae9bc7e7fcc60ed6ebaebc971269b3
Rizki01-debug <subagjamuhammadrizky@gmail.com>
2024-12-27 21:00:20 +0700

    memperbaiki codingan double

--- config.php
+++ config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php  
 // config.php  
 $host = 'localhost';  
@@ -12,19 +11,4 @@ try {
 } catch (PDOException $e) {  
     echo 'Connection failed: ' . $e->getMessage();  
 }  
-=======
-<?php  
-// config.php  
-$host = 'localhost';  
-$db = 'tubes_pbo';  
-$user = 'root';
-$pass = ''; 
-
-try {  
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);  
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
-} catch (PDOException $e) {  
-    echo 'Connection failed: ' . $e->getMessage();  
-}  
->>>>>>> 07fc2b772ffb2aba057972666d699f2386d1ea67
 ?>
